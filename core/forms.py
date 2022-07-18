60b6d05b8274d400c6c581b0ecb2f837d1dcc8a7
Rafael Martins <volny@outlook.com>
2022-07-18 18:56:19 -0300

    COMMIT FORMS

--- core/forms.py
+++ core/forms.py
@@ -7,8 +7,9 @@ class AddWorkedHours(forms.ModelForm):
         model = HoraTrabalhada
         fields = ('worker', 'date', 'worked_hours', 'description')
         labels = {'worker': 'Trabalhador', 'date':'Data', 'worked_hours':'Horas Trabalhadas', 'description':'Descrição'}
-        widgets {
+        widgets = {
             'worker': forms.TextInput(attrs={'class': 'form-control', 'type':'hidden'}),
             'date': forms.DateInput(attrs={'class':'form-control'}),
-            
+            'worked_hours': forms.TimeInput(format='%H:%M' attrs={'class':'form-control', 'type':'time'}),
+            'description': forms.TextInput(attrs={'class':'form-control'}),
         }
