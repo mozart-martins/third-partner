bb5ce121303eec06104b33c07f465f4438347a11
Mozart Dias Martins <mozart.dias.martins@gmail.com>
2022-08-20 20:03:00 -0300

    Changing files organization

--- thirdpartner/settings.py
+++ thirdpartner/settings.py
@@ -17,7 +17,7 @@ import environ
 
 env = environ.Env(
     # set casting, default value
-    DEBUG=(bool, False)
+    DEBUG=(bool, True)
 )
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
